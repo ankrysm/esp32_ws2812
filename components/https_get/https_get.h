9497fb312686b49e7caa575becdcfaf31ca8cb5c
Andreas Krysmanski <krysmanski@tup.com>
2022-10-04 22:49:21 +0200

    daily save

---
+++ components/https_get/https_get.h
@@ -0,0 +1,21 @@
+/*
+ * https_get.h
+ *
+ *  Created on: Oct 4, 2022
+ *      Author: andreas
+ */
+
+#ifndef COMPONENTS_HTTPS_GET_HTTPS_GET_H_
+#define COMPONENTS_HTTPS_GET_HTTPS_GET_H_
+
+
+typedef enum {
+	HCT_INIT,
+	HCT_READING,
+	HCT_FINISH
+} t_https_callback_todo;
+
+typedef int (*https_get_callback)(t_https_callback_todo todo, uint8_t **buf, size_t *buf_len);
+esp_err_t https_get(char *url, https_get_callback callback);
+
+#endif /* COMPONENTS_HTTPS_GET_HTTPS_GET_H_ */
